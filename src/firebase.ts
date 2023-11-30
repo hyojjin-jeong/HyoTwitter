9c810b35182bfaf5bbb3bc5954cf67e3f1a25d4d
정효진 <jeonghyojin@keunhyojing.local>
2023-12-01 01:03:30 +0900

    firebase 프로젝트 초기화

---
+++ src/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDF0vkKV-iOgP0kmaMLXM9WtnlJ0Eu5gVM",
+  authDomain: "hyotwitter-65102.firebaseapp.com",
+  projectId: "hyotwitter-65102",
+  storageBucket: "hyotwitter-65102.appspot.com",
+  messagingSenderId: "239229712940",
+  appId: "1:239229712940:web:842aa1c50c67c9cd5aaf89"
+};
+
+
+const app = initializeApp(firebaseConfig);
